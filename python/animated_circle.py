db2c1d0fa11ca8e9b84b244eacf94ae9df73fcf5
Muchen He <mansurhe@outlook.com>
2019-06-14 20:13:47 -0700

    Update animated_circle.py
    Reduced size down to 381 bytes

--- python/animated_circle.py
+++ python/animated_circle.py
@@ -4,16 +4,15 @@ def c(s,r):
  s.clear()
  x,y,d=0,r,3-2*r
  while x<y:
-  a,b=int(x),int(y)
+  a,b,x=int(x),int(y),x+1
   for p in zip([b,b,-b,-b,a,a,-a,-a],[a,-a,a,-a,b,-b,b,-b]):s.addstr(h//2+p[0],w//2+p[1],'*')
-  x+=1
   if d>0:y,d=y-1,d+4*(x-y)+10
   else:d+=4*x+6
  s.refresh()
 def m(s):
  s.nodelay(1)
  i=1
- while i:
+ while 1:
   if s.getch()==49:break
   c(s,9*math.sin(i)+15)
   i+=.1
