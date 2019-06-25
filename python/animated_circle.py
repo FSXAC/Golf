276248046f0dbe8da2e69a88c8d16ef67d479fcf
Muchen He <i@muchen.ca>
2019-06-25 10:06:20 -0700

    Update animated_circle.py

--- python/animated_circle.py
+++ python/animated_circle.py
@@ -1,18 +1,13 @@
 import curses,math
-def c(s,r):
- s.clear()
- x,y,d,h,w=0,r,3-2*r,*s.getmaxyx()
- while x<y:
-  a,b,x=int(x),int(y),x+1
-  for p in zip([b,b,-b,-b,a,a,-a,-a],[a,-a,a,-a,b,-b,b,-b]):s.addstr(h//2+p[0],w//2+p[1],'*')
-  if d>0:y,d=y-1,d+4*(x-y)+10
-  else:d+=4*x+6
- s.refresh()
 def m(s):
- s.nodelay(1)
- i=1
+ s.nodelay(1);i=1
  while 1:
   if s.getch()==49:break
-  c(s,9*math.sin(i)+15)
-  i+=.1
+  r=9*math.sin(i)+15;s.clear();h,w=s.getmaxyx();x,y,d=0,r,3-2*r
+  while x<y:
+   a,b=int(x),int(y);x+=1
+   for p in zip([b,b,-b,-b,a,a,-a,-a],[a,-a,a,-a,b,-b,b,-b]):s.addstr(h//2+p[0],w//2+p[1],'*')
+   if d>0:d+=4*(x-y)+10;y-=1
+   else:d+=4*x+6
+  s.refresh();i+=.1
 curses.wrapper(m)
